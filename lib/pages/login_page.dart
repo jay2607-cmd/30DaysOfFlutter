51c272b31c099ddcef7ff83e245dfba88936c641
Jay <jaygaudani07@gmail.com>
2022-04-05 16:41:04 +0530

    day2 completed

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class loginPage extends StatelessWidget {
+  const loginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+
+          const SizedBox(
+            height: 70,
+          ),
+          const Text(
+            "Welcome :)",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(28),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: "Enter password", labelText: "Password"),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print("hi jay!");
+            },
+            child: Text("Login"),
+            style: TextButton.styleFrom(),
+          )
+        ],
+      ),
+    );
+   
+  }
+}
